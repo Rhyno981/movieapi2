31a9e5572756416945f4d4f7ce631647da5fa76a
Ryan McDowell <69727352+Rhyno981@users.noreply.github.com>
2022-12-15 14:38:52 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+import Movielist from "./movielist";
+import Searchmovie from "./Searchmovie.js";
+
+
+const App = () => {
+
+  const[searchmovie, setSearchmovie] = useState('')
+  const[movie, setMovie] = useState([])
+
+  const Requestmovie = async()=>{
+    const url = `https://www.omdbapi.com/?s=${searchmovie}&apikey=7c68506e`;
+    const response = await fetch(url);
+    const data = await response.json();
+    if(data.Search){
+    setMovie(data.Search);
+    }
+  }
+
+  useEffect(() =>{
+    Requestmovie(searchmovie);
+  }, [searchmovie])
+
+
+
+  return (
+  <div className="whole">    
+    <h1>Movie Search</h1>
+    <div className="movie">
+      <Searchmovie searchmovie={searchmovie} setSearchmovie={setSearchmovie}/>
+    </div>
+    <div className="card d-flex flex-wrap">
+    <h1></h1>
+    <Movielist movie={movie}/>
+    </div>
+    </div>
+  )
+  
+}
+
+export default App;
